3fe7007937590d54a1fc5cd03cb9536840f76860
Sujay Goswami <sg23600@github.com>
2021-11-25 11:12:38 +0530

    fixed syntax

--- src/components/Note/Note.js
+++ src/components/Note/Note.js
@@ -2,19 +2,19 @@ import React from "react"
 import { Link } from "react-router-dom"
 import "./Note.scss"
 
-const Note = ({ title, body, date }) => {
-  console.log(date)
+const Note = ({ title, body }) => {
+  // console.log(date)
   return (
     <article className="center mw5 mw6-ns hidden shadow-5 br4 mv4">
       <h1 className="f4 bg-light-blue white mv0 pv2 ba b--dashed bw3 ph3">
         <Link exact to="/notes/edit">
-          <button class="btn edit">
-            <i class="fa fa-edit"></i>
+          <button className="btn edit">
+            <i className="fa fa-edit"></i>
           </button>
         </Link>
         <Link exact to="/notes/">
-          <button class="btn del">
-            <i class="fa fa-trash"></i>
+          <button className="btn del">
+            <i className="fa fa-trash"></i>
           </button>
         </Link>
         <div className="card-head near-black">
